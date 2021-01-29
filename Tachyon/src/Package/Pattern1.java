01f219804a001a29c6e0ee931712f5108f1c23e8
vamshi <vamshi@DESKTOP-3U9MREP>
2021-01-29 10:56:15 -0800

    Files adding to repo

---
+++ Tachyon/src/Package/Pattern1.java
@@ -0,0 +1,26 @@
+package Package;
+
+public class Pattern1 
+{
+public static void main(String[] args) {
+	
+	for(int i=1;i<=7;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			System.out.print(j);
+		}
+		System.out.println();
+	}
+	
+	for(int k=6;k>=0;k--)
+	{
+		for(int l=1;l<=k;l++)
+		{
+			System.out.print(l);
+		}
+		System.out.println();
+	}
+
+}
+}
